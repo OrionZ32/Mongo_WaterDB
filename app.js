5572ca00c906afb3488a33796e656007a5308d35
Harley Garcia <hagarcia99@outlook.com>
2020-10-17 16:07:51 -0500

    Added relationship to existing documents

--- app.js
+++ app.js
@@ -23,13 +23,13 @@ const waterSchema = new mongoose.Schema({
 const Water = mongoose.model("Water", waterSchema);
 
 // saves a new water to collection
-const smart = new Water({
-  name: "Smart",
-  rating: 8,
-  review: "A really tasty water",
+const fiji = new Water({
+  name: "Fiji",
+  rating: 7,
+  review: "It's an okay water",
 });
 
-smart.save();
+fiji.save();
 
 //schema for taster collection
 const tasterSchema = new mongoose.Schema({
@@ -43,63 +43,11 @@ const tasterSchema = new mongoose.Schema({
 
 const Taster = mongoose.model("Taster", tasterSchema);
 
-//saves a new taster to collection
-const taster = new Taster({
-  name: "Mike",
-  age: 23,
-  favoriteWater: smart,
-});
-
-taster.save();
-
-//finds the collection of waters
-// Water.find(function (err, waters) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     mongoose.connection.close();
-
-//     waters.forEach(function (water) {
-//       console.log(water.name);
-//     });
-//   }
-// });
-
 //updates one document of data
-// Water.updateOne(
-//   { _id: "5f8b16e2eb32fa2484d6c09c" },
-//   { name: "Nestle" },
-//   function (err) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log("Successfully updated the document");
-//     }
-//   }
-// );
-
-//deletes one document of data
-// Water.deleteOne({ _id: "5f8b16e2eb32fa2484d6c09c" }, function (err) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("Successfully deleted document");
-//   }
-// });
-
-//deletes many documents of data
-// Water.deleteMany({ name: "Evian" }, function (err) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("Successfully deleted documents");
-//   }
-// });
-
-// Taster.deleteMany({ name: "Joe" }, function (err) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("Successfully deleted documents");
-//   }
-// });
+Taster.updateOne({ name: "Joe" }, { favoriteWater: fiji }, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully updated the document");
+  }
+});
